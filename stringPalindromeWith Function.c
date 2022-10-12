6986e3a90394854b1c3e732ba2c8aab177274513
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-10-12 10:42:25 +0530

    Create stringPalindromeWith Function.c

---
+++ stringPalindromeWith Function.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void check(char [], int); 
+int main()
+{
+    char s[1000];  
+    printf("Enter  the string : ");
+    scanf("%s",s);
+    check(s,0);
+    return 0;
+}
+
+void check(char s[], int c)
+{
+    int i,n;
+    n=strlen(s);
+    for(i=0;i<n/2;i++)  
+    {
+    	if(s[i]==s[n-i-1])
+    	c++;
+ 
+ 	}
+ 	if(c==i)
+ 	    printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+}
